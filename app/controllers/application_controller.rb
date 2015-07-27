08a09107f031d8e6c8936e7763710492cb24097c
Dylan Steck <dylancsteck@gmail.com>
2015-07-27 18:10:19 +0000

    hi

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,5 @@
 require_relative "../../config/environment"
-require_relative "../models/project.rb"
-require_relative "../models/user.rb"
+
 class ApplicationController < Sinatra::Base
-  
-  set :views, "app/views"
-  set :public, "public"
-	
-	configure do
-  enable :sessions
-		set :session_secret, "flatstarter"
-	end
-	
-	get'/' do
-		erb :index
-  end	
-	get '/projects' do
-		erb :projects
-  end
-	
 
 end
